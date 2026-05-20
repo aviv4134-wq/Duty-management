2e423f16d02afe5c3a90d832fa6bc470e02cd4d6
avraham <aviv4134@gmail.com>
2026-05-20 10:56:58 +0300

    init data of program

---
+++ duty management program/data.py
@@ -0,0 +1,52 @@
+soldiers = [
+    {
+        "id": 10001,
+        "name": "David Cohen",
+        "duties": ["Guard Duty", "Kitchen Patrol", "Weapon Cleaning"]
+    },
+    {
+        "id": 10002,
+        "name": "Michael Levi",
+        "duties": ["Patrol", "Radio Operator"]
+    },
+    {
+        "id": 10003,
+        "name": "Daniel Mizrahi",
+        "duties": ["Guard Duty", "Logistics Check"]
+    },
+    {
+        "id": 10004,
+        "name": "Ethan Peretz",
+        "duties": ["Driver", "Vehicle Maintenance"]
+    },
+    {
+        "id": 10005,
+        "name": "Benjamin Avraham",
+        "duties": ["Medic", "First Aid Instructor"]
+    },
+    {
+        "id": 10006,
+        "name": "Alexander Friedman",
+        "duties": ["Guard Duty", "Intel Analysis"]
+    },
+    {
+        "id": 10007,
+        "name": "Samuel Ashkenazi",
+        "duties": ["Kitchen Patrol", "Base Operations"]
+    },
+    {
+        "id": 10008,
+        "name": "Gabriel Ohayon",
+        "duties": ["Patrol", "Sentry Duty"]
+    },
+    {
+        "id": 10009,
+        "name": "Elijah Malka",
+        "duties": ["Armory Management", "Weapon Cleaning"]
+    },
+    {
+        "id": 10010,
+        "name": "Matthew Haddad",
+        "duties": ["Guard Duty", "Radio Operator"]
+    }
+]
