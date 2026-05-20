8b37a16556559111f5eb7c8b38836464b17b1f9a
avraham <aviv4134@gmail.com>
2026-05-20 10:58:16 +0300

    function/print menu and start main function

---
+++ duty management program/main.py
@@ -0,0 +1,19 @@
+import data
+
+def print_menu():
+    print("""MENU
+press 1 show soldiers
+press 2 add a solider
+press 3 remove a soldier
+press 4 add a duty to a soldier
+press 5 update a duty to a soldier
+press 6 show a soldier duty""")
+
+
+
+def main():
+    run_program = True
+    while run_program:
+        pass
+
+print_menu()
