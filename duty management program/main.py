5d8c41783e888e4d526b2f0d9ab3bd216d589059
avraham <aviv4134@gmail.com>
2026-05-20 11:09:18 +0300

    function/get_user_choice

--- duty management program/main.py
+++ duty management program/main.py
@@ -1,6 +1,10 @@
 import data
 
 def print_menu():
+    """print menu to user
+    parameter : none
+    return : none """
+    
     print("""MENU
 press 1 show soldiers
 press 2 add a solider
@@ -9,11 +13,14 @@ press 4 add a duty to a soldier
 press 5 update a duty to a soldier
 press 6 show a soldier duty""")
 
+def get_user_choice():
+    user_input = input('enter a number between 1 - 6 : ')
+    return user_input
+
 
 
 def main():
     run_program = True
     while run_program:
-        pass
+        print_menu()
 
-print_menu()
