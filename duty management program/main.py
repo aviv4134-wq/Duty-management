5c725c6f5d427fec4b432cbafc427a16f1a69160
avraham <aviv4134@gmail.com>
2026-05-25 19:24:33 +0300

    fix print menu

--- duty management program/main.py
+++ duty management program/main.py
@@ -10,7 +10,7 @@ press 1 show soldiers
 press 2 add a solider
 press 3 remove a soldier
 press 4 add a duty to a soldier
-press 5 update a duty to a soldier
+press 5 update status duty to a soldier
 press 6 show a soldier duty""")
 
 
